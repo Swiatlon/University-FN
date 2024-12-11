2ea47e7bdb10a4e2e40e7ca474e2e7a693a58c33
Swiatlon <przemyslaw.swiatlon@gmail.com>
2024-12-11 20:56:12 +0100

    App bar and grades ui fix

--- src/components/shared/summaryCard/styles/Styled.tsx
+++ src/components/shared/summaryCard/styles/Styled.tsx
@@ -2,18 +2,20 @@ import { Box, styled, Typography } from '@mui/material';
 
 export const CardContainer = styled(Box)(({ theme }) => ({
   display: 'flex',
+  alignItems: 'center',
   gap: theme.spacing(2),
   padding: theme.spacing(2),
   borderRadius: theme.shape.borderRadius,
   backgroundColor: theme.palette.background.paper,
 }));
 
-export const CardIconRoundedContainer = styled(Box)<{ color: string }>(({ theme, color }) => ({
+export const CardIconRoundedContainer = styled(Box)<{ color: string }>(({ color }) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
   borderRadius: '50%',
-  padding: theme.spacing(1.5),
+  width: '50px',
+  height: '50px',
   backgroundColor: color,
 }));
 
