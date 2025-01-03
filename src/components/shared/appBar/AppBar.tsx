bd74a19066a3f749db95cbb9583798d85dcd3230
Swiatlon <przemyslaw.swiatlon@gmail.com>
2025-01-03 16:44:59 +0100

    special eslint errors

--- src/components/shared/appBar/AppBar.tsx
+++ src/components/shared/appBar/AppBar.tsx
@@ -1,16 +1,21 @@
 import MenuIcon from '@mui/icons-material/Menu';
 import { Box, Typography } from '@mui/material';
 import { useTypedDispatch } from 'hooks/useStore.Hooks';
-import { useTypedMatches } from 'hooks/useTypedMatches.Hook';
 import { toggleDrawer } from 'redux/stateSlices/view/View.State.Slice';
 import AppBarConfig from './elements/AppBarConfig';
 import './styles/AppBar.scss';
+import { useTypedMatches } from 'hooks/useTypedMatches.Hook';
 
 function AppBar() {
+  var a =5;
+
+
+
+
   const dispatch = useTypedDispatch();
   const matches = useTypedMatches();
 
-  const currentMatch = matches.find(match => match.handle);
+        const currentMatch = matches.find(match => match.handle);
   const pageTitle = currentMatch?.handle?.navigation?.text;
 
   const handleToggleDrawer = () => {
